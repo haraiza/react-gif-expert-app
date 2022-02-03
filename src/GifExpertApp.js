c3e6d0379592c9890e1a4b68fb722e47a2c9ceb3
humberto-desk <humberto_araiza@outlook.com>
2022-02-03 09:00:41 -0800

    74. FetchAPI - Obtener imagenes

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,27 +1,21 @@
 import React, { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
-  // const categories = ['One Punch', 'Samurai X', 'Dragon ball'];
-
-  const [categories, setCategories] = useState([
-    'One Punch',
-    'Samurai X',
-    'Dragon ball',
-  ]);
+  const [categories, setCategories] = useState(['Dragon ball']);
 
   return (
     <>
       <h2>GifExpertApp</h2>
 
-      {/* Se envia el metodo setCategories para que lo maneje AddCategory */}
       <AddCategory setCategories={setCategories} />
       <hr />
 
       <ol>
-        {categories.map((category, i) => {
-          return <li key={category}>{category}</li>;
-        })}
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
       </ol>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GifExpertApp } from './GifExpertApp';
 
-
 import './index.css';
 
 //import reportWebVitals from './reportWebVitals';
